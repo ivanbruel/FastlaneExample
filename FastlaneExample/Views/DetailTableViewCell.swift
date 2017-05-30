12b6470042396134eba81239d6869fe637cf9454
Ivan Bruel <ivan.bruel@gmail.com>
2017-05-30 16:30:36 +0100

    created detail cell to override cell style

--- FastlaneExample/Views/DetailTableViewCell.swift
+++ FastlaneExample/Views/DetailTableViewCell.swift
@@ -15,7 +15,7 @@ final class DetailTableViewCell: UITableViewCell {
   override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
     super.init(style: UITableViewCellStyle.value1, reuseIdentifier: reuseIdentifier)
   }
-  
+
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
