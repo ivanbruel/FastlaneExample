991a2acd3fef1e15bdcbdb83879d6e50a6214b0e
Ivan Bruel <ivan.bruel@gmail.com>
2017-05-30 16:32:52 +0100

    added AppDelegate UI Testing mocking

--- FastlaneExample/App/AppDelegate.swift
+++ FastlaneExample/App/AppDelegate.swift
@@ -13,14 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow? = {
     let window = UIWindow(frame: UIScreen.main.bounds)
-    let repositoriesViewController = RepositoriesViewController(viewModel: RepositoriesViewModel(networking: .newNetworking()))
+    let networking = ProcessInfo.processInfo.environment["UI_TESTING"] == nil ?
+      GitHubNetworking.newNetworking() : GitHubNetworking.newStubbedNetworking()
+    let repositoriesViewController =
+      RepositoriesViewController(viewModel: RepositoriesViewModel(networking: networking))
     window.rootViewController = UINavigationController(rootViewController: repositoriesViewController)
     return window
   }()
 
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     window?.makeKeyAndVisible()
     return true
   }
